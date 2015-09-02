ec746f3ff5db0114277a9c467b8f3e6f9d2a0190
Maximus <elendorial@gmail.com>
2015-09-03 00:09:11 +0300

    added ShadeRec class

---
+++ ShadeRec.h
@@ -0,0 +1,37 @@
+#ifndef __SHADE_REC__
+#define __SHADE_REC__
+
+#include <vector>
+
+class Material;
+class World;
+
+
+#include "Point3D.h"
+#include "Normal.h"
+#include "Ray.h"
+#include "RGBColor.h"
+
+
+class ShadeRec 
+{
+	public:
+
+		bool hit_an_object;
+		Material* material_ptr;
+		Point3D hit_point;
+		Point3D local_hit_point;
+		Normal normal;
+		Ray ray;
+		int depth;
+		float t;
+		World& w;
+		RGBColor color;
+
+		ShadeRec(World& wr);
+
+		ShadeRec(const ShadeRec& sr);
+
+};
+
+#endif
