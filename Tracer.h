98c3013768e56c9d22f7a6253c049f7b3dae406e
Maximus <elendorial@gmail.com>
2015-09-03 18:26:46 +0300

    added Tracer class

---
+++ Tracer.h
@@ -0,0 +1,30 @@
+#ifndef __TRACER__
+#define __TRACER__
+
+#include "Constants.h"
+#include "Ray.h"
+#include "RGBColor.h"
+
+class World;
+
+class Tracer {
+
+	public:
+
+		Tracer(void);
+
+		Tracer(World* _world_ptr);
+
+		virtual ~Tracer(void);
+
+		virtual RGBColor trace_ray(const Ray& ray) const;
+
+		virtual RGBColor trace_ray(const Ray ray, const int depth) const;
+
+	protected:
+
+		World* world_ptr;
+
+};
+
+#endif
