907af6906f51be0030a9dbf9d3909a066b69a0ab
Maximus <elendorial@gmail.com>
2015-10-03 02:12:27 +0300

    license additions

--- HW3/src/Directional.cpp
+++ HW3/src/Directional.cpp
@@ -12,7 +12,14 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+This file incorporates work covered by the following copyright and permission notice:
+
+Copyright (C) Kevin Suffern 2000-2007.
+This C++ code is for non-commercial purposes only.
+This C++ code is licensed under the GNU General Public License Version 2.
+See the file COPYING.txt for the full license. */
 
 #include "Directional.h"
 
